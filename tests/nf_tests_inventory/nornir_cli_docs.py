f9ea2d9852cdc456beda1de5b98309b392ceeba4
Denis Mulyalin <d.mulyalin@gmail.com>
2024-12-01 21:21:47 +1000

    updating docs

--- tests/nf_tests_inventory/nornir_cli_docs.py
+++ tests/nf_tests_inventory/nornir_cli_docs.py
@@ -2,21 +2,18 @@ import pprint
 
 from norfab.core.nfapi import NorFab
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     nf = NorFab(inventory="inventory.yaml")
     nf.start()
-    
+
     client = nf.make_client()
-    
+
     res = client.run_job(
         service="nornir",
         task="cli",
-        kwargs={
-            "commands": ["show clock", "show hostname"],
-            "FC": "ceos-spine"              
-        }
+        kwargs={"commands": ["show clock", "show hostname"], "FC": "ceos-spine"},
     )
-    
+
     pprint.pprint(res)
-    
+
     nf.destroy()
